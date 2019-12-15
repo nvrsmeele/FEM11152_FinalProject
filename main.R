e8e82111fd4c9ecfa7d4b9d041a867a6ee955fa4
nvrsmeele <nvrsmeele@gmail.com>
2019-12-15 22:25:37 +0100

    V1.2.15 Updated code

--- main.R
+++ main.R
@@ -63,10 +63,15 @@ str(reviews)
 
 # (re-)Factor dependent variable "Stars"
 reviews$stars <- factor(reviews$stars, ordered = TRUE)
+#reviews$stars <- mapvalues(reviews$stars, from = c("1", "2", "4", "5"),
+#                               to = c("1-2", "1-2", "4-5", "4-5"))
 
-# Create df with dependent variable "Stars" as 3 classes
-reviews$stars <- mapvalues(reviews$stars, from = c("1", "2", "4", "5"),
-                           to = c("1-2", "1-2", "4-5", "4-5"))
+# Create new "Sentiment" variable
+reviews$sentiment <- reviews$stars
+
+# Create df with dependent variable "Stars" as 2 classes
+reviews$sentiment <- mapvalues(reviews$sentiment, from = c("1", "2", "3", "4", "5"),
+                     to = c("Not_awesome", "Not_awesome", "Not_awesome", "Awesome", "Awesome"))
 
 # Clean work environment to free memory
 rm(business, reviews_full, yelp_business, yelp_reviews, reviews_subset)
@@ -98,7 +103,7 @@ unistem_textDTM <- DocumentTermMatrix(text_stem,
 
 sparse <- removeSparseTerms(unistem_textDTM, 0.99) # remove sparse terms
 unistem_textDTM <- as.data.frame(as.matrix(sparse)) # convert dtm to matrix
-unistem_textDTM$stars <- reviews$stars # add dependent variable to matrix
+unistem_textDTM$sentiment <- reviews$sentiment # add dependent variable to matrix
 
 # Unigram model based on Lemmatization
 unilemm_textDTM <- DocumentTermMatrix(text_lemm,
@@ -184,7 +189,7 @@ dtm_train <- create_dtm(it_train, vectorizer)
 ##----
 ## Split unistem_textDTM randomly for training/test
 set.seed(111)
-samp <- sample.split(unistem_textDTM$stars, SplitRatio = 2/3)
+samp <- sample.split(unistem_textDTM$sentiment, SplitRatio = 2/3)
 train.uni <- subset(unistem_textDTM, samp == TRUE) # declare training set
 test.uni <- subset(unistem_textDTM, samp == FALSE) # declare test set
 
@@ -198,18 +203,18 @@ samp <- sample.split(unilemm_textDTM$stars, SplitRatio = 2/3)
 train.lemm <- subset(unilemm_textDTM, samp == TRUE)
 test.lemm <- subset(unilemm_textDTM, samp == FALSE)
 
-ytrain.lemm <- train.lemm[, 735]
-xtrain.lemm <- train.lemm[, c(1:734, 736:893)]
+ytrain.lemm <- train.lemm[, 740]
+xtrain.lemm <- train.lemm[, c(1:739, 741:898)]
 
 #----
 # DownSample
 #----
-trainDown <- downSample(xtrain.uni, ytrain.uni, yname = "stars")
+trainDown <- downSample(xtrain.uni, ytrain.uni, yname = "sentiment")
 xtrainDown.uni <- trainDown[,1:829]
 ytrainDown.uni <- trainDown[,830]
 
-ytest.uni <- test.uni[,888] # declare test response variable
-xtest.uni <- test.uni[,1:887] # declare test predictor variables
+ytest.uni <- test.uni[,830] # declare test response variable
+xtest.uni <- test.uni[,1:829] # declare test predictor variables
 
 ## Split bistem_textDTM randomly for training/test
 set.seed(150)
@@ -234,25 +239,99 @@ xtest.bi <- test.bi[,1:104] # declare test predictor variables
 ## multi-class Naive Bayes Classifier (base model)
 
 # Step1: Create NB classifier
-nb_model <- multinomial_naive_bayes(as.matrix(xtrainDown.uni), ytrainDown.uni)
+nb_model <- multinomial_naive_bayes(as.matrix(xtrainDown.uni), ytrainDown.uni, laplace = 0.5)
 
 # Step 2: Generate predictions of the NB classifier
-pred <- predict(nb_model, data = test.uni, type = "class")
+pred <- predict(nb_model, data = test.uni, method = "class")
 
 # Step 3: Create confusion matrix of NB's prediction performance
 nb_cfm <- confusionMatrix(data = pred, reference = ytrainDown.uni)
+
+#Test with VIP/LIME by using Caret
+#----
+trcontrol <- trainControl(method = "none")
+
+nb_model <- train(xtrainDown.uni, ytrainDown.uni,
+                  method = "naive_bayes",
+                  tuneGrid = data.frame(laplace = 0.5,
+                                        usekernel = FALSE,
+                                        adjust = FALSE),
+                  trControl = trcontrol)
+
+library(vip)
+
+
+vip(nb_model, num_features = 5, bar = TRUE)
+
+library(iml)
+
+# Feature importance
+predictor <- Predictor$new(nb_model, data = as.data.frame(xtest.uni),
+                           y = ytest.uni,
+                           predict.fun = function(model, newdata){
+                             
+                             predict(model, newdata)
+
+                           })
+
+
+imp <- FeatureImp$new(predictor, loss = 'ce', compare = 'ratio',
+                     n.repetitions = 20)
+
+# LIME
+library(lime)
+explainer <- lime(x = xtrainDown.uni, model = nb_model, bin_continuous = FALSE)
+
+explanation <- lime::explain (
+  xtrainDown.uni[5:6, ], # Select Jack and Rose
+  explainer    = explainer, 
+  n_labels     = 1, # Explaining a single class
+  n_features   = 4, # Returns top-4 features critical to each case
+  kernel_width = 0.5) # Select kernel width
+
+plot_features (explanation) +
+  labs (title = "LIME: Feature Importance Visualization")
+#----
+
+##----
+## SVM Classifier
+library(e1071)
+library(radiant.data)
+
+svm_model <- svm(sentiment ~., data = trainDown,
+                 type = "nu-classification", kernel = "polynomial")
+
+pred <- predict(svm_model, data = test.uni, method = "class")
+
+svm_cfm <- confusionMatrix(data = pred, reference = ytrainDown.uni)
+
+W <- t(svm_model$coefs) %*% svm_model$SV
+weights.df <- as.data.frame(t(W), row.names = NULL)
+weights.df <- weights.df %>% rownames_to_column("word")
+names(weights.df)[names(weights) == "V1"] <- "w"
+
+weights.df %>% group_by(V1 < 0) %>%
+  top_n(20, abs(V1)) %>%
+  ungroup() %>%
+  mutate(word = reorder(word, V1)) %>%
+  ggplot(aes(word, V1, fill = V1 < 0 )) +
+  geom_col(show.legend = F) +
+  coord_flip() + 
+  ylab("w") +
+  scale_fill_manual(values = c("orange", "blue"))
 
 ##----
 ## Random Forest Classifier
 
 # Step 1: Run initial Random Forest model
 set.seed(200)
-rf_model <- randomForest(xtrain.uni, ytrain.uni, mtry = 29, replace = TRUE,
+rf_model <- randomForest(xtrainDown.uni, ytrainDown.uni, mtry = 29, replace = TRUE,
                          importance = TRUE, ntree = 75, do.trace = TRUE,
                          control = rpart.control(minsplit = 2, cp = 0))
 
 pred <- predict(rf_model, data = test.uni, type = "class")
-rf_cfm <- confusionMatrix(data = pred, reference = ytrain.uni)
+rf_cfm <- confusionMatrix(data = pred, reference = ytrainDown.uni)
+
 
 # Step 2: Find OOB error convergence to determine 'best' ntree
 plot(rf_model$err.rate[,1], type="l", xlab = "Number of bootstrap samples",
@@ -295,9 +374,9 @@ rf_testerr <- round(rf_best$test[["err.rate"]][500,1], digits = 4)
 ##----
 ## XGBoost Classifier
 
-xgbtrain <- xgb.DMatrix(data = as.matrix(xtrain.uni), label = as.numeric(ytrain.uni)-1)
+xgbtrain <- xgb.DMatrix(data = as.matrix(xtrainDown.uni), label = as.numeric(ytrainDown.uni)-1)
 
-numberOfClasses <- length(unique(ytrain.uni))
+numberOfClasses <- length(unique(ytrainDown.uni))
 xgb_params <- list("objective" = "multi:softprob",
                    "eval_metric" = "mlogloss",
                    "num_class" = numberOfClasses)
@@ -314,13 +393,14 @@ cv_model <- xgb.cv(params = xgb_params,
 
 OOF_prediction <- data.frame(cv_model$pred) %>%
   mutate(max_prob = max.col(., ties.method = "last"),
-         label = ytrain.uni)
+         label = ytrainDown.uni)
 
 OOF_prediction$label <- mapvalues(OOF_prediction$label, from = c("1-2", "3", "4-5"), to = c("1", "2", "3"))
 
 confusionMatrix(factor(OOF_prediction$max_prob),
                 factor(OOF_prediction$label),
                 mode = "everything")
+
 
 
 ##----
@@ -333,16 +413,12 @@ library(reticulate)
 reticulate::use_python("/opt/anaconda3/envs/r-base/bin")
 
 xtrain.norm <- normalize(as.matrix(xtrain.uni))
-ytrain.bin <- to_categorical(as.numeric(ytrain.uni)-1, 5)
-ytest.bin <- to_categorical(as.numeric(ytest.uni)-1, 5)
+ytrain.bin <- to_categorical(as.numeric(ytrain.uni)-1, 3)
+ytest.bin <- to_categorical(as.numeric(ytest.uni)-1, 3)
 
 model <- keras_model_sequential() %>% 
-  layer_dense(units = 32, activation = "relu", input_shape = c(499)) %>%
-  layer_dropout(0.25) %>%
-  layer_dense(units = 16, activation = "relu") %>%
-  layer_dropout(0.25) %>%
-  layer_dense(units = 8, activation = "relu") %>%
-  layer_dense(units = 5, activation = "softmax")
+  layer_dense(units = 16, activation = "relu", input_shape = c(829)) %>%
+  layer_dense(units = 3, activation = "softmax")
 
 model %>% compile(
   optimizer = 'adam',
@@ -351,10 +427,10 @@ model %>% compile(
 )
 
 trained_model <- model %>% fit(
-  xtrain.norm,
+  as.matrix(xtrain.uni),
   ytrain.bin,
   epochs = 50,
-  batch_size = 512,
+  batch_size = 650,
   validation_split = 0.2,
 )
 
